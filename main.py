b2e8aa07d315ca06bdd54b5b413b3580dd674ed4
Se7enBit <58233834+Se7enBit@users.noreply.github.com>
2023-04-12 20:22:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
+from website import create_app
 
+app = create_app()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'rwrharwgngwrdgrwogrwmrg string'
-    return app
-
+if __name__ == '__main__':
+    app.run(debug=True)
