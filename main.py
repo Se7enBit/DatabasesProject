4cae11f6c78e745a68cc6c050a329f6b4bd2fa23
Se7enBit <nick.oik.2002@gmail.com>
2023-04-13 20:56:19 +0300

    tutorial finished

--- main.py
+++ main.py
@@ -5,4 +5,3 @@ app = create_app()
 if __name__ == '__main__':  # if we run the file it starts
     # debug mode: every time we change it restarts the server
     app.run(debug=True)
-    print("Hello World")
