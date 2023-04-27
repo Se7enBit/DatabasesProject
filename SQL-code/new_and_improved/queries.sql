4fe6ddae59b9a37e0f671537ac433c84b2c1cb46
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2023-04-27 21:25:02 +0300

    added some queries

--- SQL-code/new_and_improved/queries.sql
+++ SQL-code/new_and_improved/queries.sql
@@ -3,3 +3,16 @@ SELECT writer.*  FROM writer  INNER JOIN book_writer ON writer.id = book_writer.
 
 -- Search for books per writer
 SELECT book.title FROM book JOIN book_writer ON book.id = book_writer.book_id where book_writer.writer_id = 13;
+
+-- Search for all distinct writers that have
+-- written books in a particular category
+SELECT DISTINCT writer.* FROM writer JOIN book_writer ON book_writer.writer_id = writer.id JOIN book ON book_writer.book_id = book.id WHERE FIND_IN_SET('poetry', book.category) > 0;
+
+-- Search for the writer who has written the 
+-- most books
+SELECT writer.*, COUNT(book_writer.book_id) AS NUM_books
+       FROM writer
+       INNER JOIN book_writer ON writer.id = book_writer.writer_id
+       GROUP BY writer.id
+       ORDER BY NUM_books DESC
+       LIMIT 1;
