449d7038c278e8a5bb894b4ebd99c32fd0040157
Se7enBit <nick.oik.2002@gmail.com>
2023-05-18 17:20:01 +0300

    peter avgerinos will try to run it

--- SQL-code/new_and_improved/queries.sql
+++ SQL-code/new_and_improved/queries.sql
@@ -1,39 +1,47 @@
 -- Search for writers per book
-SELECT writer.*  FROM writer  INNER JOIN book_writer ON writer.id = book_writer.writer_id  WHERE book_writer.book_id = 12;
-
+SELECT writer.*
+FROM writer
+    INNER JOIN book_writer ON writer.id = book_writer.writer_id
+WHERE book_writer.book_id = 12;
 -- Search for books per writer
-SELECT book.title FROM book JOIN book_writer ON book.id = book_writer.book_id where book_writer.writer_id = 13;
-
+SELECT book.title
+FROM book
+    JOIN book_writer ON book.id = book_writer.book_id
+where book_writer.writer_id = 13;
 -- Search for all distinct writers that have
 -- written books in a particular category
-SELECT DISTINCT writer.* FROM writer JOIN book_writer ON book_writer.writer_id = writer.id JOIN book ON book_writer.book_id = book.id WHERE FIND_IN_SET('poetry', book.category) > 0;
-
+SELECT DISTINCT writer.*
+FROM writer
+    JOIN book_writer ON book_writer.writer_id = writer.id
+    JOIN book ON book_writer.book_id = book.id
+WHERE FIND_IN_SET('poetry', book.category) > 0;
 -- Search for the writer who has written the 
 -- most books
-SELECT writer.*, COUNT(book_writer.book_id) AS NUM_books
-       FROM writer
-       INNER JOIN book_writer ON writer.id = book_writer.writer_id
-       GROUP BY writer.id
-       ORDER BY NUM_books DESC
-       LIMIT 1;
-
+SELECT writer.*,
+    COUNT(book_writer.book_id) AS NUM_books
+FROM writer
+    INNER JOIN book_writer ON writer.id = book_writer.writer_id
+GROUP BY writer.id
+ORDER BY NUM_books DESC
+LIMIT 1;
 -- Select all books that a user has rented
 select book.*
-    from book
-    join book_rental
-    ON book.id = book_rental.book_id
-    where book_rental.app_user_id = 10
+from book
+    join book_rental ON book.id = book_rental.book_id
+where book_rental.app_user_id = 10
     and book_rental.rental_status in ("rented", "late to return", "returned");
-
 -- Select all books in a specific category
-SELECT title FROM book WHERE FIND_IN_SET('drama', category);
-
+SELECT title
+FROM book
+WHERE FIND_IN_SET('drama', category);
 -- Select all books with similar titles to the prompt
-SELECT * FROM book WHERE title LIKE "%ΥΠΟΛΟΓΙΣΤΙΚΗ%";
-
+SELECT *
+FROM book
+WHERE title LIKE "%ΥΠΟΛΟΓΙΣΤΙΚΗ%";
 -- Select all users in possession of a book that is late to return
-SELECT app_user.first_name, app_user.last_name, book_rental.rental_datetime
-        FROM app_user
-        JOIN book_rental
-        ON book_rental.app_user_id = app_user.id
-        WHERE book_rental.rental_status = "late to return";
+SELECT app_user.first_name,
+    app_user.last_name,
+    book_rental.rental_datetime
+FROM app_user
+    JOIN book_rental ON book_rental.app_user_id = app_user.id
+WHERE book_rental.rental_status = "late to return";
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, url_for
 from flask_login import login_required, current_user
 from . import db
-import json, base64
+import json, base64, os
 
 views = Blueprint("views", __name__)
 
@@ -23,15 +23,19 @@ def books():
     else:
         user_id = current_user.get_id()
         cur = db.connection.cursor()
-        cur.execute("SELECT b.title, COUNT(bcps.id) AS copy_count, b.image FROM app_user AS au JOIN school AS s ON au.school = s.id JOIN book_copies_per_school AS bcps ON s.id = bcps.school_id JOIN book AS b ON bcps.book_id = b.id WHERE au.id = %s GROUP BY b.title", [user_id])
+        cur.execute("SELECT b.title, COUNT(bcps.id) AS copy_count, b.image, b.id FROM app_user AS au JOIN school AS s ON au.school = s.id JOIN book_copies_per_school AS bcps ON s.id = bcps.school_id JOIN book AS b ON bcps.book_id = b.id WHERE au.id = %s GROUP BY b.title", [user_id])
         rows = cur.fetchall()
         cur.close()
         
-        """
         modified_rows=[]
+        image_url=[]
+        ids=[]
         for row in rows:
+            ids.append(row[3]-1)
+        for index, row in enumerate(rows):
             row_list = list(row)
             image_blob = row_list[2]
+            print(image_blob)
             if image_blob:
                 # Convert the image BLOB to Base64 encoding
                 image_base64 = base64.b64encode(image_blob).decode('utf-8')
@@ -40,14 +44,16 @@ def books():
                 row_list[2] = None
             modified_row = tuple(row_list)
             modified_rows.append(modified_row)
-        """
+            image_url.append(url_for('static', filename=f'images/{ids[index]}.png'))
+        
+        
 
         cur = db.connection.cursor()
         cur.execute("SELECT s.appellation FROM app_user AS au JOIN school AS s ON au.school = s.id WHERE au.id = %s", [user_id])
         school = cur.fetchone()
         cur.close()
 
-        return render_template("books.html", user = current_user, rows=rows, school = school)
+        return render_template("books.html", user = current_user, rows=modified_rows, image_url=image_url, school = school)
 
 @views.route("/queries", methods=["GET", "POST"])
 @login_required
